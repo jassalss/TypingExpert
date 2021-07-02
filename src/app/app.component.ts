28c72fa989f8e46f772867b887e330b6dac5a63b
Simerpreet S Jassal <simerpreetjassal13@gmail.com>
2021-07-02 10:41:03 -0600

    almost completed the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,32 @@ import { lorem } from 'faker';
 export class AppComponent {
   sentence = lorem.sentence();
   typedText = '';
+  WordsLeft = 0;
+  wrongLetters = 0;
 
   setTypeLetter(value: string) {
     this.typedText = value;
+
+    this.difference(value, this.sentence);
+    console.log('total', this.sentence.length);
+    console.log('wordLeft', this.WordsLeft);
+    console.log('Wrong Letters', this.wrongLetters);
   }
   compare(actualLetter: string, typedLetter: String) {
     if (!typedLetter) {
       return '';
     }
     return actualLetter === typedLetter ? 'correct' : 'incorrect';
-    
   }
+  difference = (firstStr: string, secondStr: string) => {
+    this.wrongLetters = 0;
+    this.WordsLeft = this.sentence.length;
+    for (let i = 0; i < firstStr.length; i++) {
+      if (firstStr[i] != secondStr[i]) {
+        this.wrongLetters++;
+      } else {
+        this.WordsLeft--;
+      }
+    }
+  };
 }
